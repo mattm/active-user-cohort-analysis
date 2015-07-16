13c58ae9f2b0c8917a4b4b5a39b7ad9c354bf78b
Matt Mazur <matthew.h.mazur@gmail.com>
2015-07-16 16:30:52 -0400

    Split script up into multiple functions

--- active-users.r
+++ active-users.r
@@ -1,6 +1,61 @@
 CSV_PATH = "data/test-data.csv"
 CSV_SEPARATOR = "\t"
 
+Run <- function() {
+	activities <- LoadActivityData()
+	data <- AnalyzeActiveUserCohorts(activities)
+	PlotActiveUserCohorts(data)
+}
+
+LoadActivityData <- function() {
+	activities <- read.csv(CSV_PATH, sep = CSV_SEPARATOR,
+		col.names = c("user.id", "date"), header = FALSE)
+	activities$date <- as.Date(activities$date)
+	activities$cohort <- format(activities$date, "%Y-%m")
+	activities
+}
+
+AnalyzeActiveUserCohorts <- function(activities) {
+	users <- GetActiveUserCohorts(activities)
+
+	# Figure out all of the cohorts that we need to analyze
+	# TODO: What happens if there is month without any data?
+	cohorts <- sort(unique(users$cohort))
+
+	# Construct a data frame that we can supply to ggplot
+	signup.cohorts <- vector()
+	activity.cohorts <- vector()
+	active.users <- vector()
+	for (signup.cohort in cohorts) {
+
+		# Figure out which users signed up in this cohort
+		signup.user.ids <- users[users$cohort == signup.cohort, "user.id"]
+		for (activity.cohort in cohorts) {
+
+			# Figure out which activities those users performed in every other cohort
+			# including cohorts before they signed up (required for ggplot stacking)
+			user.activities <- activities[activities$user.id %in% signup.user.ids
+				& activities$cohort == activity.cohort, ]
+
+			# If a user performed multiple activities in a cohort, only count it once
+			unique.users <- unique(user.activities$user.id)
+
+			# Keep track of each vector so we can construct the data frame afterwards
+			# TODO: There is probably a more elegant way to do this
+			signup.cohorts <- append(signup.cohorts, signup.cohort)
+			activity.cohorts <- append(activity.cohorts, activity.cohort)
+			active.users <- append(active.users, length(unique.users))
+		}
+	}
+
+	data.frame(signup.cohorts, as.factor(activity.cohorts), active.users)
+}
+
+# We determine which cohort each user belongs to based on his first activity
+GetActiveUserCohorts <- function(activities) {
+	aggregate(cohort ~ user.id, activities, min)
+}
+
 # TODO: Figure out how to prevent ggplot from rendering a thin line for
 # cohorts that have zero active users in a month
 PlotActiveUserCohorts <- function(data) {
@@ -18,44 +73,4 @@ PlotActiveUserCohorts <- function(data) {
 	print(graph)
 }
 
-activities <- read.csv(CSV_PATH, sep = CSV_SEPARATOR,
-	col.names = c("user.id", "date"), header = FALSE)
-activities$date <- as.Date(activities$date)
-activities$cohort <- format(activities$date, "%Y-%m")
-
-# We determine which cohort each user belongs to based on his first activity
-users <- aggregate(cohort ~ user.id, activities, min)
-
-# Figure out all of the cohorts that we need to analyze
-# TODO: What happens if there is month without any data?
-cohorts <- sort(unique(users$cohort))
-
-# Construct a data frame that we can supply to ggplot
-signup.cohorts <- vector()
-activity.cohorts <- vector()
-active.users <- vector()
-for (signup.cohort in cohorts) {
-
-	# Figure out which users signed up in this cohort
-	signup.user.ids <- users[users$cohort == signup.cohort, "user.id"]
-	for (activity.cohort in cohorts) {
-
-		# Figure out which activities those users performed in every other cohort
-		# including cohorts before they signed up (required for ggplot stacking)
-		user.activities <- activities[activities$user.id %in% signup.user.ids
-			& activities$cohort == activity.cohort, ]
-
-		# If a user performed multiple activities in a cohort, only count it once
-		unique.users <- unique(user.activities$user.id)
-
-		# Keep track of each vector so we can construct the data frame afterwards
-		# TODO: There is probably a more elegant way to do this
-		signup.cohorts <- append(signup.cohorts, signup.cohort)
-		activity.cohorts <- append(activity.cohorts, activity.cohort)
-		active.users <- append(active.users, length(unique.users))
-	}
-}
-
-data <- data.frame(signup.cohorts, as.factor(activity.cohorts), active.users)
-
-PlotActiveUserCohorts(data)
+Run()
